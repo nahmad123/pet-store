65580ac6a667eb47c375c982c6a252721dd415a8
nahmad123 <ahmadn20@hotmail.com>
2024-08-31 19:34:05 -0700

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (customer_first_name, customer_last_name, customer_email) VALUES ('Tom', 'Hardy', 'thardy@123.com');
+INSERT INTO customer (customer_first_name,customer_last_name, customer_email) VALUES ('Lisa', 'Gee', 'lgee@123.com');
+INSERT INTO customer (customer_first_name, customer_last_name, customer_email) VALUES ('Norma', 'Jean', 'njean@123.com');
